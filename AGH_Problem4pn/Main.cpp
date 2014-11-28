4e66a6ca902c18696bb6861cbfd6d27af694a98d
czmi3l <czmiel1224@gmail.com>
2014-11-28 23:51:42 +0100

    1

---
+++ AGH_Problem4pn/Main.cpp
@@ -0,0 +1,49 @@
+#include "Valve.h"
+
+string status2symbol(Valve& valve){
+	return valve.IsOpen() == 1 ? "Otwarty" : "Zamkniety";
+}
+
+int main() {
+
+	Valve v1;
+	Valve v2(Valve::Opened);
+	Valve v3(Valve::Opened);
+
+	cout << "Po stworzeniu: Zawor v1 " << status2symbol(v1)
+		<< " zawor v2 " << status2symbol(v2)
+		<< " zawor v3 " << status2symbol(v3) << endl;
+
+	v2.open();
+	cout << "Po otwarciu v2: Zawor v1 " << status2symbol(v1)
+		<< " zawor v2 " << status2symbol(v2)
+		<< " zawor v3 " << status2symbol(v3) << endl;
+
+	v3.open();
+	cout << "Po otwarciu v3:  Zawor v1 " << status2symbol(v1)
+		<< " zawor v2 " << status2symbol(v2)
+		<< " zawor v3 " << status2symbol(v3) << endl;
+
+	v1.open();
+	cout << "Po otwarciu v1: Zawor v1 " << status2symbol(v1)
+		<< " zawor v2 " << status2symbol(v2)
+		<< " zawor v3 " << status2symbol(v3) << endl;
+
+	Valve v4(Valve::Opened);
+	cout << "Po dodaniu otwartego v4:  Zawor v1 " << status2symbol(v1)
+		<< " zawor v2 " << status2symbol(v2)
+		<< " zawor v3 " << status2symbol(v3)
+		<< " zawor v4 " << status2symbol(v4) << endl;
+
+	v1.open();
+	cout << "Po otwarciu v1: Zawor v1 " << status2symbol(v1)
+		<< " zawor v2 " << status2symbol(v2)
+		<< " zawor v3 " << status2symbol(v3)
+		<< " zawor v4 " << status2symbol(v4) << endl;
+	cin.get();
+	Valve::clean();
+
+	return 0;
+}
+
+
---
+++ AGH_Problem4pn/Valve.h
@@ -0,0 +1,75 @@
+#include "Lista.h"
+
+class Valve
+{
+public:
+	enum Flags{
+		Opened,
+		Closed
+	};
+	Valve();
+	Valve(Flags);
+	~Valve();
+	Valve(const Valve&);
+	bool IsOpen();
+	Valve& operator=(const Valve valve);
+	void open();
+	void close();
+	static void clean();
+	static Lista < Valve > list;
+private:
+	Flags flag;
+};
+
+Lista<Valve> Valve::list;
+
+Valve::Valve()
+{
+	flag = Closed;
+	Valve::list.push_back(this);
+}
+
+Valve::Valve(Flags fla)
+{
+	Valve::list.push_back(this);
+
+	clean();
+	flag = Opened;
+}
+
+Valve::~Valve()
+{
+}
+
+Valve::Valve(const Valve& valve){
+	flag = valve.flag;
+}
+
+bool Valve::IsOpen(){
+	if (flag == Opened)
+		return true;
+	return false;
+}
+
+Valve& Valve::operator=(const Valve valve){
+	flag = valve.flag;
+	return *this;
+}
+
+void Valve::open(){
+	Lista<Valve>::Iterator iter = Valve::list.begin();
+	clean();
+	flag = Opened;
+}
+
+void Valve::close(){
+	flag = Closed;
+}
+
+void Valve::clean(){
+	Lista<Valve>::Iterator iter = Valve::list.begin();
+	for (iter; iter != Valve::list.end(); iter++)
+	{
+		(*iter).close();
+	}
+}
